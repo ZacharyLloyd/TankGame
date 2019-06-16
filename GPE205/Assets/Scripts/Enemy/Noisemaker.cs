2db236cf7f1cbea52f2f2eadeeedc4e91b1db952
ZacharyLloyd <46659989+ZacharyLloyd@users.noreply.github.com>
2019-06-16 14:56:42 -0700

    Added in senses and noisemaker scripts

---
+++ GPE205/Assets/Scripts/Enemy/Noisemaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noisemaker : MonoBehaviour
+{
+    public float PlayerVolume = 0; //Setting the player volume to be adjusted by designers
+    public float SlowDownVolume = 0.016f; //How the volume fades away to nothing
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Each frame noise gets lower until it hits zero/nothing
+        if (PlayerVolume > 0)
+        {
+            PlayerVolume -= SlowDownVolume;
+        }
+    }
+}
